b139b9af3ff5058ac2c22b8c8884e3caf6a3bd0c
ruban-s <rubans013@gmail.com>
2023-09-21 14:22:52 +0530

    Menu changes and varnish cache purger changes

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -1,39 +1,48 @@
 <?php
+
 namespace W3cert\CacheCleaner\Controller\Adminhtml\Index;
 
-use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action;
+use Magento\Framework\App\Cache\Manager as CacheManager;
+use Magento\CacheInvalidate\Model\PurgeCache;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-    protected $cacheManager;
-    protected $varnishPurger;
+    private $cacheManager;
+    private $varnishPurger;
+    private $cacheTypeList;
 
     public function __construct(
         Action\Context $context,
-        \Magento\Framework\App\Cache\Manager $cacheManager,
-        \Magento\CacheInvalidate\Model\PurgeCache $varnishPurger
+        CacheManager $cacheManager,
+        PurgeCache $varnishPurger,
+        TypeListInterface $cacheTypeList
     ) {
         $this->cacheManager = $cacheManager;
         $this->varnishPurger = $varnishPurger;
+        $this->cacheTypeList = $cacheTypeList;
         parent::__construct($context);
     }
 
     public function execute()
     {
         try {
-            $types = array_keys($this->cacheManager->getAvailableTypes());
+            // Clean all Magento cache types
+            $types = array_keys($this->cacheTypeList->getTypes());
             $this->cacheManager->clean($types);
             
-            // Purge Varnish Cache
-            $this->varnishPurger->purgeAll();
-
+            // Purge Varnish Cache for all pages
+            $this->varnishPurger->sendPurgeRequest('.*');
+    
             $this->messageManager->addSuccessMessage(__('Cache has been cleaned.'));
         } catch (\Exception $e) {
             $this->messageManager->addErrorMessage($e->getMessage());
         }
-
+    
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         return $resultRedirect->setPath('adminhtml/dashboard/index');
     }
+    
 }
